49a8a49d21589ef0596d6f090356d576ff55a831
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2024-09-18 13:12:33 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import CubeLoader from '../components/CubeLoader';
+
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <CubeLoader />
+    </div>
+  );
+}
